b807f9f1a4612679a84f6c7bf39d6af757fdf168
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-01-20 17:11:52 +0300

    Complete static version

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,8 +1,13 @@
 function SearchBox() {
   return (
     <div className="searchbox">
-      <input className="searchbox__input" />
-      <span className="searchbox__icon">Arrow</span>
+      <input
+        className="searchbox__input"
+        placeholder="Search for any IP address or domain"
+      />
+      <div className="searchbox__icon-container">
+        <img className="searchbox__icon" src="images/icon-arrow.svg" />
+      </div>
     </div>
   );
 }
