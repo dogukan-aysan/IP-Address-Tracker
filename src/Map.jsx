b528be2bb59075ee84905adfd5049556007f1a59
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-01-21 08:55:54 +0300

    Get current position of the user on the initial page load

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,28 +1,52 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 function Map() {
-  const [mapPosition, setMapPosition] = useState([51.505, -0.09]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [position, setPosition] = useState([]);
+  useEffect(() => {
+    if (!navigator.geolocation)
+      return setError("Your browser does not support geolocation");
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition([pos.coords.latitude, pos.coords.longitude]);
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }, []);
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
   return (
-    <div className="map">
-      <MapContainer
-        center={mapPosition}
-        zoom={13}
-        scrollWheelZoom={true}
-        style={{ height: "100%" }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-    </div>
+    <>
+      {position.length > 0 && (
+        <div className="map">
+          <MapContainer
+            style={{ height: "100%" }}
+            center={position}
+            zoom={13}
+            scrollWheelZoom={false}
+            zoomControl={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      )}
+    </>
   );
 }
 
