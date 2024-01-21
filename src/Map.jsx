9ae83b494b6cdf255f41265d6491de9613e48086
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-01-21 06:07:12 +0300

    Include a map with the Leaflet library

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 function Map() {
-  return <div className="map">Map</div>;
+  const [mapPosition, setMapPosition] = useState([51.505, -0.09]);
+  return (
+    <div className="map">
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        style={{ height: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 }
 
 export default Map;
