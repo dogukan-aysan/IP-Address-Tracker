8dabc26fce458f60b62a4927ce484b2f795345f4
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-01-22 08:03:53 +0300

    Implement the functionality of searching for an ip address by react-hook-form and bootstrap

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,14 +1,40 @@
+import { useForm } from "react-hook-form";
+
 function SearchBox() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const myHandleSubmit = () => {
+    alert("SUBMITTEDDD");
+  };
   return (
-    <div className="searchbox">
+    <form className="searchbox" onSubmit={handleSubmit(myHandleSubmit)}>
       <input
-        className="searchbox__input"
+        className={`form-control searchbox__input${
+          errors.ip ? " is-invalid" : ""
+        }`}
         placeholder="Search for any IP address or domain"
+        {...register("ip", {
+          required: "This field is required",
+          pattern: {
+            value:
+              /(?:(?:25[0-5]|2[0-4]\d|[01]?\d?\d{1})\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d?\d{1})/g,
+            message: "Please provide a valid ip address",
+          },
+        })}
       />
-      <div className="searchbox__icon-container">
+      {
+        <span className="invalid-feedback searchbox__error-message">
+          {errors?.ip?.message}
+        </span>
+      }
+
+      <button type="submit" className="searchbox__icon-container">
         <img className="searchbox__icon" src="images/icon-arrow.svg" />
-      </div>
-    </div>
+      </button>
+    </form>
   );
 }
 
